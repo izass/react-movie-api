fa962163a1394830ea7823933af09baa477fdda2
Isaac Moreira Barbosa <izass1312@gmail.com>
2021-06-24 11:53:08 -0300

    Refactor routes tree

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
 import React from 'react';
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  useHistory
+} from 'react-router-dom'
+
+import Landing from './pages/landing'
+import Movie from './pages/movie'
+import Search from './pages/search';
+import NavBar from './components/NavBar';
+
 import './styles.css'
-import Routes from './routes'
+
 
 function App() {
+  const history = useHistory()
   return (
-    <div>
-      <Routes />
-    </div>
+    <Router>
+      <Route path="/" component={NavBar}/>
+      <Switch history={history}>
+        <Route exact path="/" component={Landing}/>
+        <Route path="/movies/:id" component={Movie}/>
+        <Route path="/search/:query" component={Search}/>
+      </Switch>
+    </Router>
   );
 }
 
