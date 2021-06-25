f84c2e9d2d38b770e1a725f4e7fdde94595434b2
Isaac Moreira Barbosa <izass1312@gmail.com>
2021-06-25 11:55:05 -0300

    Refactor api calls

--- src/pages/SearchResults/index.js
+++ src/pages/SearchResults/index.js
@@ -1,8 +1,6 @@
 import React, { useEffect, useState } from "react";
 
-// import "./style.css";
-
-import apiQuery from "../../services/apiQuery";
+import { searchMovies } from "../../services/api";
 import Grid from "../../components/Grid";
 
 import { Container } from "./styles.js";
@@ -13,8 +11,8 @@ function SearchResults(props) {
   useEffect(() => {
     async function fetchData() {
       const { query } = props.match.params;
-      const response = await apiQuery.get(query);
-      setMovies(response.data.results);
+      const response = await searchMovies(query);
+      setMovies(response);
     };
 
     fetchData()
--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,29 @@
 import axios from 'axios'
 
-const api = axios.create({ 
-    baseURL: "https://api.themoviedb.org/3/movie/"
+const api = axios.create({
+    baseURL: "https://api.themoviedb.org/3/"
  });
 
- export default api;
+ async function searchMovies(querry) {
+  try {
+    const response = await api.get(`search/movie?api_key=344db5c6830b31f30c9a1b6ba00bde07&language=en-US&page=1&include_adult=false&query=${querry}`)
+    return response.data.results
+  } catch (error) {
+    return error
+  }
+ }
+
+ async function getMovie(id) {
+  try {
+    const response = await api.get(`movie/${id}?api_key=344db5c6830b31f30c9a1b6ba00bde07`)
+    return response.data
+  } catch (error) {
+    return error
+  }
+ }
+
+ export {
+   searchMovies,
+   getMovie
+ };
+
