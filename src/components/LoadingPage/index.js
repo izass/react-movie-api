5c1f8ff50f03d1a9aae9a23790faf5ef0919fa91
Isaac Moreira Barbosa <izass1312@gmail.com>
2021-06-28 16:57:13 -0300

    Create LoadingPage component

---
+++ src/components/LoadingPage/index.js
@@ -0,0 +1,13 @@
+import { Container } from './styles'
+
+import LoadImage from '../../assets/gifs/play_movie_preview.gif'
+
+const LoadingPage = () => {
+  return (
+    <Container>
+      <img alt="loading" src={LoadImage}/>
+    </Container>
+  )
+}
+
+export default LoadingPage
